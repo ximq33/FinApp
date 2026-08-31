d5a4a9015de1a1afbffc58f732157fed1b5ef69c
Szymon Kęcik <110922975+ximq33@users.noreply.github.com>
2026-08-31 14:11:45 +0200

    fix(remortgage): update form component and package dependencies

--- force-app/main/default/lwc/remortgageForm/remortgageForm.js
+++ force-app/main/default/lwc/remortgageForm/remortgageForm.js
@@ -1,15 +1,60 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import createLeadWithRules from '@salesforce/apex/RemortgageController.createLeadWithRules';
+import { CurrentPageReference } from 'lightning/navigation';
 
 export default class RemortgageForm extends LightningElement {
     isSubmitted = false; 
     errorMessage = '';
+    @track trackingSource = null;
+    @track socialMediaTracking = null;
 
-    handleSubmit(event) {
-    
+    @wire(CurrentPageReference)
+    getStateParameters(currentPageReference) {
+        if (currentPageReference && currentPageReference.state) {
+            const sourcedBy = currentPageReference.state.sourcedBy;
+        
+            if (sourcedBy) {
+                this.mapTrackingValues(sourcedBy);
+            }
+        }
+    }
+
+    mapTrackingValues(sourcedByValue) {
+        switch (sourcedByValue) {
+            case 'bankPartner':
+                this.trackingSource = 'Bank Partner';
+                this.socialMediaTracking = null;
+                break;
+            case 'mortgagePartner':
+                this.trackingSource = 'Mortgage Partner';
+                this.socialMediaTracking = null;
+                break;
+            case 'linkedin':
+                this.trackingSource = 'Social Media';
+                this.socialMediaTracking = 'LinkedIn';
+                break;
+            default:
+                this.trackingSource = null;
+                this.socialMediaTracking = null;
+        }
+        
+        console.log('Tracking Source:', this.trackingSource);
+        console.log('Social Media:', this.socialMediaTracking);
+    }
+
+    // TODO Custom metadata
+
+handleSubmit(event) {
         event.preventDefault(); 
         
-        const fields = event.detail.fields;
+        const fields = { ...event.detail.fields };
+
+        if (this.trackingSource) {
+            fields.Tracking_Source__c = this.trackingSource;
+        }
+        if (this.socialMediaTracking) {
+            fields.Social_Media_Tracking__c = this.socialMediaTracking;
+        }
 
         createLeadWithRules({ newLead: fields })
             .then(() => {
